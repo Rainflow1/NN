7caab161bd2d86bb803fd0f4b76ed4f629a97c67
kungfuszifu <92547578+kungfuszifu@users.noreply.github.com>
2024-03-24 20:59:27 +0100

    init

--- src/UCF_CC_50.py
+++ src/UCF_CC_50.py
@@ -120,48 +120,6 @@ class UCF_CC_50_Dataset:
                     writer.writerows(targetPoints)
         pass
 
-    def gaussian_filter_density(self, img, points):
-        '''
-        This code use k-nearst, will take one minute or more to generate a density-map with one thousand people.
-
-        points: a two-dimension list of pedestrians' annotation with the order [[col,row],[col,row],...].
-        img_shape: the shape of the image, same as the shape of required density-map. (row,col). Note that can not have channel.
-
-        return:
-        density: the density-map we want. Same shape as input image but only has one channel.
-
-        example:
-        points: three pedestrians with annotation:[[163,53],[175,64],[189,74]].
-        img_shape: (768,1024) 768 is row and 1024 is column.
-        '''
-        img_shape = [img.size[0], img.size[1]]
-        print("Shape of current image: ", img_shape, ". Totally need generate ", len(points), "gaussian kernels.")
-        density = np.zeros(img_shape, dtype=np.float32)
-        gt_count = len(points)
-        if gt_count == 0:
-            return density
-
-        leafsize = 2048
-        # build kdtree
-        tree = scipy.spatial.KDTree(points.copy(), leafsize=leafsize)
-        # query kdtree
-        distances, locations = tree.query(points, k=4)
-
-        print('generate density...')
-        for i, pt in enumerate(points):
-            pt2d = np.zeros(img_shape, dtype=np.float32)
-            if int(pt[1]) < img_shape[0] and int(pt[0]) < img_shape[1]:
-                pt2d[int(pt[1]), int(pt[0])] = 1.
-            else:
-                continue
-            if gt_count > 1:
-                sigma = (distances[i][1] + distances[i][2] + distances[i][3]) * 0.1
-            else:
-                sigma = np.average(np.array(img.size)) / 2. / 2.  # case: 1 point
-            density += gaussian_filter(pt2d, sigma, mode='constant')
-        print('done.')
-        return density
-
     def intersectionAreaRatio(self, box, fragments):
         ret = 0
 
@@ -239,13 +197,6 @@ class CCDataset(Dataset):
         with open(pointsPath, 'r', newline='') as csvFile:
             reader = csv.reader(csvFile, quoting=csv.QUOTE_NONNUMERIC)
             points = [(int(x), int(y)) for x, y in reader]
-
-        #plt.figure(1)
-        #plt.imshow(image)
-        #plt.figure(2)
-        #plt.imshow(target)
-        #plt.show()
-        #print(f'{len(points)}, {np.sum(np.array(target))}')
 
         image = T.to_tensor(T.to_grayscale(image, 3))
         target = T.to_tensor(target)
